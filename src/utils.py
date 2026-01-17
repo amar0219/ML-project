a8516f9b4cf6a5170d5043506e6e70abe9ae3763
Amar Barade <amarbarade6@gmail.com>
2026-01-18 01:45:11 +0530

    hyperparameter tuning

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import pickle
 import dill
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     """Saves a Python object to a file using pickle."""
@@ -18,13 +19,19 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models,param):
     """Evaluates multiple regression models and returns their R2 scores."""
     try:
         report = {}
         for i in range(len(models)):
             model = list(models.values())[i]
+            para = param[list(models.keys())[i]]
+
+            gs=GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, y_train)
             # Train the model
+            # model.fit(X_train, y_train)
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
             y_train_pred = model.predict(X_train)
 
